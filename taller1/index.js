4dec8d0bc8a31f92b38f7cbe9fb74e5feb31b8dc
ingquimbay <quimbayc@javeriana.edu.co>
2018-02-11 22:52:44 -0500

    taller 1

---
+++ taller1/index.js
@@ -0,0 +1,11 @@
+function cambialo(element) {
+  element.style.color = "white";
+  element.style.backgroundColor = "#FFAE27";
+  element.style.borderstyle = "solid";
+  element.style.bordercolor = "282FBF";
+  element.style.borderradius = "15px";
+  element.style.boxshadow = "10px 10px 10px gray";
+  element.style.width = "500px";
+  element.style.height = "200px";
+  element.childNodes [0].nodeValue = "Se ha cambiado el estylo del texto";
+}
